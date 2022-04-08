7aa184c77211296d0d9c7ca7f37b062574290120
Cesar Acual <augustoacual@gmail.com>
2022-04-07 21:24:36 -0600

    Modelo para controlar la app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer')
+const Busquedas = require('./models/busquedas')
 
 const main = async () => {
+  const busquedas = new Busquedas()
   let opt = ''
 
   do {
@@ -8,7 +10,24 @@ const main = async () => {
 
     switch (opt) {
       case 1:
-        console.log('Buscar ciudad')
+        // Mostrar mensaje
+        const lugar = await leerInput('Ciudad: ')
+        console.log(lugar)
+
+        // Buscar lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        // Mostrar resultados
+        console.log('\nInformación de la ciudad\n'.green)
+        console.log('Ciudad:')
+        console.log('Lat:')
+        console.log('Lng:')
+        console.log('Temperatura:')
+        console.log('Mínima:')
+        console.log('Máxima:')
         break
 
       case 2:
