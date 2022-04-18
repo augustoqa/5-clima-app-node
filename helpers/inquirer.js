338aa479e71c6029cbf31fdd19646500af75e99a
Cesar Acual <chechaacual@gmail.com>
2022-04-18 12:39:57 -0600

    Listar países de forma interactiva

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -66,12 +66,12 @@ const leerInput = async (message) => {
   return desc
 }
 
-const listadoTareasBorrar = async (tareas = []) => {
-  const choices = tareas.map((tarea, i) => {
+const listarLugares = async (lugares = []) => {
+  const choices = lugares.map((lugar, i) => {
     const idx = `${i + 1}`.green
     return {
-      value: tarea.id,
-      name: `${idx} ${tarea.desc}`,
+      value: lugar.id,
+      name: `${idx} ${lugar.nombre}`,
     }
   })
 
@@ -84,7 +84,7 @@ const listadoTareasBorrar = async (tareas = []) => {
     {
       type: 'list',
       name: 'id',
-      message: 'Borrar',
+      message: 'Sellecione lugar:',
       choices,
     },
   ]
@@ -133,7 +133,7 @@ module.exports = {
   inquirerMenu,
   pausa,
   leerInput,
-  listadoTareasBorrar,
+  listarLugares,
   confirmar,
   listadoCheckList,
 }
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
-const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer')
+const {
+  leerInput,
+  inquirerMenu,
+  pausa,
+  listarLugares,
+} = require('./helpers/inquirer')
 const Busquedas = require('./models/busquedas')
 
 const main = async () => {
@@ -12,20 +17,22 @@ const main = async () => {
     switch (opt) {
       case 1:
         // Mostrar mensaje
-        const lugar = await leerInput('Ciudad: ')
-        await busquedas.ciudad(lugar)
+        const termino = await leerInput('Ciudad: ')
 
         // Buscar lugares
+        const lugares = await busquedas.ciudad(termino)
 
         // Seleccionar el lugar
+        const id = await listarLugares(lugares)
+        const lugarSeleccionado = lugares.find((l) => l.id === id)
 
         // Clima
 
         // Mostrar resultados
         console.log('\nInformación de la ciudad\n'.green)
-        console.log('Ciudad:')
-        console.log('Lat:')
-        console.log('Lng:')
+        console.log('Ciudad:', lugarSeleccionado.nombre)
+        console.log('Lat:', lugarSeleccionado.lat)
+        console.log('Lng:', lugarSeleccionado.lng)
         console.log('Temperatura:')
         console.log('Mínima:')
         console.log('Máxima:')
